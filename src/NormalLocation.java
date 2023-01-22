943222ab16a2775b86c0492c16a32444d1ebfdad
Duha YILDIRIM <1duhayildirim@gmail.com>
2023-01-22 19:11:15 +0300

    normal location

---
+++ src/NormalLocation.java
@@ -0,0 +1,10 @@
+public abstract class NormalLocation extends Location {
+    public NormalLocation(Player player, String name) {
+        super(player, name);
+    }
+
+    @Override
+    boolean onLocation() {
+        return true;
+    }
+}
