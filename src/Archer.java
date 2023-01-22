5580e7cf82d9d0cc10ae35e3c99b7a4b14850af6
Duha YILDIRIM <1duhayildirim@gmail.com>
2023-01-22 18:52:55 +0300

    subclasses

---
+++ src/Archer.java
@@ -0,0 +1,5 @@
+public class Archer extends Character {
+    public Archer() {
+        super(2, "Okçu", 7, 18, 20);
+    }
+}
---
+++ src/Chevalier.java
@@ -0,0 +1,5 @@
+public class Chevalier extends Character {
+    public Chevalier() {
+        super(3, "Şövalye", 8, 24, 5);
+    }
+}
